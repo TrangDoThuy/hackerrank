3dbe80f80e191d79c1b858c4844c6749c56e91c3
Emma Thuy Trang DO <30380242+TrangDoThuy@users.noreply.github.com>
2020-10-15 21:42:10 +0800

    added problem Array Manipulation

---
+++ src/ArrayManipulation.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArrayManipulation {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long [] arr = new long[n];
+        long length = queries.length;
+        for(int i=0;i<length;i++){
+            int start = queries[i][0]-1;
+            int end = queries[i][1]-1;
+            int num = queries[i][2];
+            arr[start]+=num;
+            if(end+1<n){
+                arr[end+1]-=num;
+            }
+        }
+        long max=arr[0];
+        long result=0;
+        for(int i=0;i<n;i++){
+            result+=arr[i];
+            if(result>max){
+                max=result;
+            }
+        }
+        return max;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
